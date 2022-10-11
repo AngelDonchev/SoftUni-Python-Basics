ad41c09165c423e7d9eeb21957fae900795986d0
Angel Donchev <angel.m.donchev@gmail.com>
2022-10-11 17:56:14 +0300

    Create basketball.py

---
+++ basketball.py
@@ -0,0 +1,9 @@
+tax = int(input())
+shoes = tax - tax*4/10
+clothes = shoes - shoes*2/10
+ball = clothes/4
+accessiories = ball/5
+
+total_cost = tax + shoes + clothes + ball + accessiories
+
+print(total_cost)
